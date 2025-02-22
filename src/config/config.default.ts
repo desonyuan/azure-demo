5fbeea814888b73daf9bacf99d9829d73c90ef4c
desonyuan <42114557@qq.com>
2025-02-22 22:03:14 +0800

    fix

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,8 +1,8 @@
 import { MidwayConfig } from '@midwayjs/core';
 
-export default {
-  console.log(process.env.PORT,'process.env.PORT');
+console.log(process.env.PORT, 'process.env.PORT');
 
+export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1740231949393_8242',
   koa: {
