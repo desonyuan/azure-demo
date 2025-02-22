d30c4c6b5971bf9b8d30ee7dfb063444b2b78adb
desonyuan <42114557@qq.com>
2025-02-22 21:54:05 +0800

    fix

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,6 +4,6 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1740231949393_8242',
   koa: {
-    port: 7001,
+    port: process.env.PORT || 7001,
   },
 } as MidwayConfig;
